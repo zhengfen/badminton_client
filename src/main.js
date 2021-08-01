291c49964d346b3e906009d146bbc2f3dc1e0c48
zhengfen <tozhengfen@gmail.com>
2021-08-01 21:18:16 +0200

    club show updated

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import router from './router'
 
 import "@/styles/index.scss"; // global css
 
+
+
 window.axios = require("axios");
 window.axios.defaults.baseURL = location.hostname == 'localhost' ? 'http://localhost:3000/api/competitions': 'http://localhost:3000/api/competitions';
 
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 const app = createApp(App)
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
 
 export default app
--- src/views/ClubShow.vue
+++ src/views/ClubShow.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-if="club">
     <h4>{{ club.name }}</h4>
-    <div class="card">
+    <!-- responsables -->
+    <div class="card mb-5">
       <div class="card-header">
         Responsables
       </div>
@@ -16,13 +17,59 @@
             <td>{{ responsable.user.first_name }} </td>
             <td>{{ responsable.user.contact && responsable.user.contact.address }}</td>
             <td>{{ responsable.user.contact && responsable.user.contact.npa }}</td>
-            <td>{{ responsable.user.contact && responsable.user.contact.city }}</td>  
-            <td>{{ responsable.user.contact && responsable.user.contact.phone }}</td>  
-            <td>{{ responsable.user.email }}</td>  
+            <td>{{ responsable.user.contact && responsable.user.contact.city }}</td>
+            <td>{{ responsable.user.contact && responsable.user.contact.phone }}</td>
+            <td>{{ responsable.user.email }}</td>
           </tr>
         </table>
       </div>
     </div>
+
+    <!-- teams -->
+    <h5>Teams</h5>
+    <div
+      class="card"
+      v-for="team in teams"
+      :key="team.id"
+    >
+      <div class="card-header">
+        {{ team.name }}
+      </div>
+      <div class="card-body">
+        <table class="tab">
+          <tr>
+            <td>Group</td>
+            <td>{{ team.group.name }}</td>
+          </tr>
+          <tr>
+            <td>Captain</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Members</td>
+            <td>
+              <buttun
+                class="btn btn-sm btn-primary"
+                @click="show_player_add_modal(team.id)"
+              >
+                <i class="fas fa-plus"></i>
+              </buttun>
+            </td>
+          </tr>
+          <tr></tr>
+        </table>
+
+      </div>
+    </div>
+
+    <el-dialog
+      title="addPlayer"
+      v-model="playerAddVisible"
+      width="30%"
+    >
+      <player-form mode="new" :team_id="team_id" />
+    </el-dialog>
+
   </div>
 </template>
 
@@ -31,8 +78,11 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      club: null, 
-      responsables: []
+      club: null,
+      responsables: [],
+      teams: [],
+      team_id: undefined,  // team_id for player form
+      playerAddVisible: false,
     }
   },
   created() {
@@ -46,7 +96,12 @@ export default {
           console.log('club data', data);
           this.club = data.club;
           this.responsables = data.responsables;
+          this.teams = data.teams;
         })
+    },
+    show_player_add_modal(team_id) {
+      this.player_add_team_id = team_id;
+      this.playerAddVisible = true; 
     }
   }
 }
